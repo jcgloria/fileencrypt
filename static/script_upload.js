d44de88d54d1af2df006ac51efec163ad7255674
jcgloria <96juancg@gmail.com>
2023-04-01 18:38:52 -0500

    small changes

--- static/script_upload.js
+++ static/script_upload.js
@@ -1,9 +1,13 @@
 function handleFileSelect() {
-    const fileInput = document.getElementById('formFile');
-    const fileName = fileInput.files[0].name;
-    document.getElementById("formRoute").value = fileName;
+  const fileInput = document.getElementById('formFile');
+  const fileName = fileInput.files[0].name;
+  document.getElementById("formRoute").value = fileName;
+}
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  if (form.checkValidity()) {
+    progressBar = document.getElementById("progressBar")
+    progressBar.removeAttribute("hidden")
   }
-document.getElementById('submitBtn').addEventListener('click', ()=>{
-  progressBar = document.getElementById("progressBar")
-  progressBar.removeAttribute("hidden")
-});
+})
