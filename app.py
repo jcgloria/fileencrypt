53af1469934284a9d6dc7c7a3c365e032daa6e21
jcgloria <96juancg@gmail.com>
2023-04-21 10:05:30 +0100

    improve comments in code

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ import json
 import encrypt_utils
 import os
 
+# Initialize the Flask app and enable CORS
 app = Flask(__name__)
 CORS(app)
 
-# load credentials and create s3 client
+# load AWS credentials and create s3 client.
+# Warning: The app will not work if the credentials are not defined in the env.json file
 credentials = json.loads(open('env.json').read())
 s3 = boto3.client('s3', aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key'], region_name=credentials['region'])
 
-#add ./keys and ./client_files if they don't exist
+#add ./keys and ./client_files directories if they don't exist
 if not os.path.exists("./keys"):
     os.makedirs("./keys")
 if not os.path.exists("./client_files"):
     os.makedirs("./client_files")
 
-# Get the list of buckets
-bucketList = []
+bucketList = [] # This is an array that contains the names of all the user's buckets.
+
+# Get the list of buckets and populate the bucketList array
 for bucket in s3.list_buckets()['Buckets']:
     bucketList.append(bucket['Name'])
 if len(bucketList) > 0:
@@ -27,8 +30,10 @@ if len(bucketList) > 0:
 else:
     currentBucket = ""
 
-# Get the list of keys
-keyList = []
+
+keyList = [] # This is an array that contains the names of all the available encryption keys.
+
+# Get the list of keys and populate the keyList array
 files = os.listdir("./keys")
 for file in files:
     if file.endswith(".key"):
@@ -38,14 +43,19 @@ if len(keyList) > 0:
 else:
     currentKey = ""
 
+# Route that redirects the user to the current bucket if they go to the root URL.
 @app.route('/')
 def index():
     return redirect("/" + currentBucket + "/")
 
+# Route that renders the settings page.
 @app.route('/settings')
 def settings():
     return render_template('settings.html', keyList=keyList, currentKey=currentKey)
 
+# Route that uploads a file to S3 via a POST request. 
+# The file is encrypted beforehand using the specified key. 
+# The route just renders the upload.html template if it is a GET request
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     msg = ""
@@ -62,7 +72,9 @@ def upload():
             msg = "Error uploading file"
     return render_template('upload.html', currentBucket=currentBucket, bucketList=bucketList, keyList=keyList, currentKey=currentKey, message=msg)
 
-# Prepare file for download. Decrypt it and save it in the client_files folder
+# Route that handles download requests. 
+# A download request consists of downloading a file from S3 and decrypting it with a specified key.
+# The key is obtained from the file's metadata in S3. 
 @app.route("/downloadRequest", methods=['POST'])
 def downloadRequest():
     body = request.get_json()
@@ -82,14 +94,17 @@ def downloadRequest():
         f.write(decrypted)
     return json.dumps({"file": fileName}), 200
 
-# Download the file from the client_files folder
+# Sends an encrypted file to the user. 
+# The file is encrypted beforehand using the "downloadRequest" route
 @app.route("/downloadFile")
 def downloadFile():
     # get the get params
     file = request.args.get('file')
     return send_file('./client_files/'+file, as_attachment=True)
 
-# Dynamic navigation to any directory in any bucket
+# Route that handles any S3 directory in the URL.
+# If the directory does not exist, a 404 error is returned
+# Uses the helper function get_objects_by_dir
 @app.route('/<path:dirPath>')
 def any_route(dirPath):
     files = get_objects_by_dir(dirPath)
@@ -97,6 +112,9 @@ def any_route(dirPath):
         abort(404)
     return render_template('index.html', files=files, bucketList=bucketList, currentBucket=currentBucket, dir_string=dirPath)
 
+# Route to generate a new random key. 
+# The key is saved in the ./keys folder
+# If this route is called via a GET, the user is redirected to the settings page
 @app.route('/generateKey', methods=['GET','POST'])
 def generateKey():
     if request.method == 'GET':
@@ -107,6 +125,9 @@ def generateKey():
         keyList.append(keyName + ".key")
         return render_template('settings.html', keyList=keyList, currentKey=currentKey, message="Key generated successfully")
 
+# Route to import a key to the application via a POST request.
+# The key is saved in the ./keys folder
+# If this route is called via a GET, the user is redirected to the settings page
 @app.route('/importKey', methods=['GET','POST'])
 def importKey():
     if request.method == 'GET':
@@ -118,6 +139,8 @@ def importKey():
         keyList.append(keyName)
         return render_template('settings.html', keyList=keyList, currentKey=currentKey, message="Key imported successfully")
 
+# Helper function that gets the list of files in a specific directory within S3. 
+# A directory has the format: <bucket name>/<path to directory>
 def get_objects_by_dir(dir):
     dirs = dir.split("/")
     bucketName = dirs[0]
@@ -144,6 +167,7 @@ def get_objects_by_dir(dir):
     else:
         return []
 
+# Helper function that parses the objects returned by the S3 API and formats it for the front end.
 def parseObjects(objects, route):
     results = []
     for object in objects:
@@ -160,6 +184,12 @@ def parseObjects(objects, route):
     results.sort(key=lambda x: x['isFolder'], reverse=True)
     return results
 
+# Helper function that encrypts and uploads a file to S3.
+# Parameters:
+#  f: file data (array of bytes)
+#  key: encryption key name
+#  bucket: bucket name
+#  route: route to upload the file to (example: folder1/image.jpg) 
 def handle_uploaded_file(f, key, bucket, route):
     if route[0] == "/":
         route = route[1:]
@@ -171,6 +201,8 @@ def handle_uploaded_file(f, key, bucket, route):
         print(e)
         return False
 
+# Helper function that converts a number of bytes to a human readable format.
+# Example: 1024 returns "1 KB"
 def convert_size(size_bytes):
     units = ["Bytes", "KB", "MB", "GB", "TB"]
     i = 0
